dc5746ef27d06e5fd514dd688b0bfb69ee34aa07
신동규 <donggyu9410@gmail.com>
2025-04-19 22:37:46 +0900

    feat: add schema

--- schemas.py
+++ schemas.py
@@ -1,21 +1,37 @@
 from marshmallow import Schema, fields
 
+
 class PlainItemSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
-class PlainStoreScheme(Schema):
+
+class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
+    name = fields.Str()
+
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
-class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, load_only=True)
-    store = fields.Nested(PlainStoreScheme(), dump_only=True)
 
-class StoreScheme(PlainStoreScheme):
+class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
